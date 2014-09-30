5d7e3e00694cb88593ff9bdf4b9e8c76ff72dea6
Jason Grimes <jason@grimesit.com>
2014-09-30 16:23:25 -0400

    Add a micro-pager at the top of the photostream on mobile screen sizes.

--- web/js/paginator.js
+++ web/js/paginator.js
@@ -2,4 +2,12 @@ $(function() {
     $('.paginator-select-page').on('change', function() {
         document.location = $(this).val();
     });
+    $('.paginator-select-page')
+        .on('focus', function() {
+            $(this).css('font-size', '16px');
+        })
+        .on('blur', function() {
+            $(this).css('font-size', '');
+        })
+    ;
 });
